20b08ea1e5b262dfd8bb349fa56fe3bdb4308d19
fpetranzan <francesco.petranzan@gmail.com>
2024-04-07 19:52:57 +0200

    created the JwtService.java class

---
+++ src/main/java/com/fpetranzan/security/service/JwtService.java
@@ -0,0 +1,41 @@
+package com.fpetranzan.security.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+	@Value("${application.security.jwt.secret-key}")
+	private String secretKey;
+
+	public String extractUserEmail(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts
+			.parser()
+			.verifyWith(getSignInKey())
+			.build()
+			.parseSignedClaims(token)
+			.getPayload();
+	}
+
+	private SecretKey getSignInKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+}
