37368f6ff229780f55ad103dd6bf7c40530bf393
fpetranzan <francesco.petranzan@gmail.com>
2024-04-11 21:42:37 +0200

    added saving the token on the authentication service

--- src/main/java/com/fpetranzan/security/services/AuthenticationService.java
+++ src/main/java/com/fpetranzan/security/services/AuthenticationService.java
@@ -3,6 +3,9 @@ package com.fpetranzan.security.services;
 import com.fpetranzan.security.auth.AuthenticationRequest;
 import com.fpetranzan.security.auth.AuthenticationResponse;
 import com.fpetranzan.security.auth.RegisterRequest;
+import com.fpetranzan.security.token.Token;
+import com.fpetranzan.security.token.TokenRepository;
+import com.fpetranzan.security.token.TokenType;
 import com.fpetranzan.security.user.Role;
 import com.fpetranzan.security.user.User;
 import com.fpetranzan.security.user.UserRepository;
@@ -13,11 +16,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
 	private final UserRepository userRepository;
+	private final TokenRepository tokenRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final JwtService jwtService;
 	private final AuthenticationManager authenticationManager;
@@ -29,9 +35,10 @@ public class AuthenticationService {
 			.password(passwordEncoder.encode(request.getPassword()))
 			.role(Role.USER)
 			.build();
-		final String jwtToken = jwtService.generateToken(user);
-
 		userRepository.save(user);
+
+		final String jwtToken = jwtService.generateToken(user);
+		saveUserToken(user, jwtToken);
 
 		return AuthenticationResponse.builder()
 			.token(jwtToken)
@@ -43,8 +50,36 @@ public class AuthenticationService {
 
 		User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
 		final String jwtToken = jwtService.generateToken(user);
+		revokeAllUserToken(user);
+		saveUserToken(user, jwtToken);
+
 		return AuthenticationResponse.builder()
 			.token(jwtToken)
 			.build();
+	}
+
+	private void revokeAllUserToken(User user) {
+		List<Token> validUserTokens = tokenRepository.findAllValidTokensByUser(user.getId());
+
+		if(validUserTokens.isEmpty()) {
+			return;
+		}
+
+		validUserTokens.forEach(token -> {
+			token.setExpired(Boolean.TRUE);
+			token.setRevoked(Boolean.TRUE);
+		});
+		tokenRepository.saveAll(validUserTokens);
+	}
+
+	private void saveUserToken(User user, String jwtToken) {
+		final Token token = Token.builder()
+			.user(user)
+			.token(jwtToken)
+			.tokenType(TokenType.BEARER)
+			.expired(Boolean.FALSE)
+			.revoked(Boolean.FALSE)
+			.build();
+		tokenRepository.save(token);
 	}
 }
