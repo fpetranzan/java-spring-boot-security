9a13b94a85ffd414acd54ad945d78f8a7e5759ba
fpetranzan <francesco.petranzan@gmail.com>
2024-04-11 23:09:55 +0200

    created constants class

---
+++ src/main/java/com/fpetranzan/security/constants/AuthConstants.java
@@ -0,0 +1,9 @@
+package com.fpetranzan.security.constants;
+
+public final class AuthConstants {
+
+	public static class AUTHORIZATION_TYPE {
+		public static final String BEARER = "Bearer ";
+	}
+
+}
