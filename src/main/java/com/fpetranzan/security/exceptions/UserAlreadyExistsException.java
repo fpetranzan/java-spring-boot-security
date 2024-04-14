8c6ba99cb1bf31f701205e2c8d548c969d0b8315
fpetranzan <francesco.petranzan@gmail.com>
2024-04-14 16:48:16 +0200

    created user exists exception

---
+++ src/main/java/com/fpetranzan/security/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.fpetranzan.security.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+	public UserAlreadyExistsException(String message) {
+		super(message);
+	}
+}
