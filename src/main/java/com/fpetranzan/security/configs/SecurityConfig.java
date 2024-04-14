daf0886ce6ea4eac9a4b10a07a5037e2ed24d5df
fpetranzan <francesco.petranzan@gmail.com>
2024-04-14 15:34:01 +0200

    added management and admin endpoint to security configuration

--- src/main/java/com/fpetranzan/security/configs/SecurityConfig.java
+++ src/main/java/com/fpetranzan/security/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,9 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+import static com.fpetranzan.security.user.Role.ADMIN;
+import static com.fpetranzan.security.user.Role.MANAGER;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthFilter;
@@ -29,6 +34,12 @@ public class SecurityConfig {
 				request
 					.requestMatchers("/api/v1/auth/**")
 					.permitAll()
+
+					.requestMatchers("/api/v1/management/**")
+					.hasAnyRole(ADMIN.name(), MANAGER.name())
+
+					.requestMatchers("/api/v1/admin/**")
+					.hasRole(ADMIN.name())
 
 					.anyRequest()
 					.authenticated()
