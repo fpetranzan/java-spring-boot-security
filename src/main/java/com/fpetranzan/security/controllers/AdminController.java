e6ea5ba3e9a05be596dbc163533526563330ed21
fpetranzan <francesco.petranzan@gmail.com>
2024-04-14 15:29:46 +0200

    created management and admin controller

---
+++ src/main/java/com/fpetranzan/security/controllers/AdminController.java
@@ -0,0 +1,38 @@
+package com.fpetranzan.security.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+	@GetMapping
+	@PreAuthorize("hasAuthority('admin:read')")
+	public String get() {
+		return "GET:: Admin Controller";
+	}
+
+	@PutMapping
+	@PreAuthorize("hasAuthority('admin:update')")
+	public String put() {
+		return "PUT:: Admin Controller";
+	}
+
+	@PostMapping
+	@PreAuthorize("hasAuthority('admin:create')")
+	public String post() {
+		return "POST:: Admin Controller";
+	}
+
+	@DeleteMapping
+	@PreAuthorize("hasAuthority('admin:delete')")
+	public String delete() {
+		return "DELETE:: Admin Controller";
+	}
+}
---
+++ src/main/java/com/fpetranzan/security/controllers/ManagementController.java
@@ -0,0 +1,38 @@
+package com.fpetranzan.security.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/management")
+public class ManagementController {
+
+	@GetMapping
+	@PreAuthorize("hasAnyAuthority('admin:read','management:read')")
+	public String get() {
+		return "GET:: Management Controller";
+	}
+
+	@PutMapping
+	@PreAuthorize("hasAnyAuthority('admin:update','management:update')")
+	public String put() {
+		return "PUT:: Management Controller";
+	}
+
+	@PostMapping
+	@PreAuthorize("hasAnyAuthority('admin:create','management:create')")
+	public String post() {
+		return "POST:: Management Controller";
+	}
+
+	@DeleteMapping
+	@PreAuthorize("hasAnyAuthority('admin:delete','management:delete')")
+	public String delete() {
+		return "DELETE:: Management Controller";
+	}
+}
