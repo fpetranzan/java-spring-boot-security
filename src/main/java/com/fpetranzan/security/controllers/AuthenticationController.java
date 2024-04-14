cd461559597b5ffe1326991dd0db7324ff6f67ac
fpetranzan <francesco.petranzan@gmail.com>
2024-04-14 12:44:06 +0200

    added change password endpoint

--- src/main/java/com/fpetranzan/security/controllers/AuthenticationController.java
+++ src/main/java/com/fpetranzan/security/controllers/AuthenticationController.java
@@ -2,7 +2,9 @@ package com.fpetranzan.security.controllers;
 
 import com.fpetranzan.security.auth.AuthenticationRequest;
 import com.fpetranzan.security.auth.AuthenticationResponse;
+import com.fpetranzan.security.auth.ChangePasswordRequest;
 import com.fpetranzan.security.auth.RegisterRequest;
+import com.fpetranzan.security.service.UserService;
 import com.fpetranzan.security.services.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -21,6 +24,7 @@ import java.io.IOException;
 public class AuthenticationController {
 
 	private final AuthenticationService authenticationService;
+	private final UserService userService;
 
 	@PostMapping("/register")
 	public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
@@ -35,5 +39,11 @@ public class AuthenticationController {
 	@PostMapping("/refresh-token")
 	public void refresh(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		authenticationService.refreshToken(request, response);
+	}
+
+	@PostMapping("/change-password")
+	public ResponseEntity<ChangePasswordRequest> changePassword(@RequestBody ChangePasswordRequest request, Principal connectedUser) throws IOException {
+		userService.changePassword(request, connectedUser);
+		return ResponseEntity.ok().build();
 	}
 }
