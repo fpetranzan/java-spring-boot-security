8f88cc4c6ee83255bfd024b45d5dc8f803871e10
fpetranzan <francesco.petranzan@gmail.com>
2024-04-14 16:48:36 +0200

    added exceptions on handler

--- src/main/java/com/fpetranzan/security/controllers/AuthenticationControllerExceptionHandler.java
+++ src/main/java/com/fpetranzan/security/controllers/AuthenticationControllerExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.fpetranzan.security.controllers;
 
 import com.fpetranzan.security.exceptions.InvalidAuthTokenException;
+import com.fpetranzan.security.exceptions.UserAlreadyExistsException;
 import com.fpetranzan.security.exceptions.UserNotFoundForAuthException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -12,6 +14,16 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @ControllerAdvice
 public class AuthenticationControllerExceptionHandler {
+
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex, WebRequest request) {
+		return ResponseEntity.status(UNAUTHORIZED).body(ex.getMessage());
+	}
+
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
+		return ResponseEntity.status(UNAUTHORIZED).body(ex.getMessage());
+	}
 
 	@ExceptionHandler(InvalidAuthTokenException.class)
 	public ResponseEntity<String> handleInvalidAuthTokenException(InvalidAuthTokenException ex, WebRequest request) {
