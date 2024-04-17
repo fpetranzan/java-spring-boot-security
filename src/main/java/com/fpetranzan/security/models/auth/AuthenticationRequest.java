094102a678a86433604956e7d179d4a1c5ef3ef3
fpetranzan <francesco.petranzan@gmail.com>
2024-04-17 21:26:23 +0200

    added validation annotation to AuthenticationRequest

--- src/main/java/com/fpetranzan/security/models/auth/AuthenticationRequest.java
+++ src/main/java/com/fpetranzan/security/models/auth/AuthenticationRequest.java
@@ -1,5 +1,9 @@
 package com.fpetranzan.security.models.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +15,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequest {
 
+	@Email(message = "Email does not have a valid format")
+	@NotEmpty(message = "Email is mandatory")
+	@NotBlank(message = "Email is mandatory")
 	private String email;
+	@Size(min = 8, message = "Password should be 8 characters long minimum")
+	@NotEmpty(message = "Password is mandatory")
+	@NotBlank(message = "Password is mandatory")
 	private String password;
 }
