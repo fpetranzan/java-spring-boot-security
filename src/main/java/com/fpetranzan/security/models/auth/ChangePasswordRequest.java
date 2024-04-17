f7c16b02d4b35a5028ea290de5c34b04088aae87
fpetranzan <francesco.petranzan@gmail.com>
2024-04-17 21:26:40 +0200

    added validation annotation to ChangePasswordRequest

--- src/main/java/com/fpetranzan/security/models/auth/ChangePasswordRequest.java
+++ src/main/java/com/fpetranzan/security/models/auth/ChangePasswordRequest.java
@@ -1,5 +1,8 @@
 package com.fpetranzan.security.models.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChangePasswordRequest {
 
+	@NotEmpty(message = "Current Password is mandatory")
+	@NotBlank(message = "Current Password is mandatory")
 	private String currentPassword;
+	@Size(min = 8, message = "Password should be 8 characters long minimum")
+	@NotEmpty(message = "New Password is mandatory")
+	@NotBlank(message = "New Password is mandatory")
 	private String newPassword;
+	@Size(min = 8, message = "Password should be 8 characters long minimum")
+	@NotEmpty(message = "Confirmation Password is mandatory")
+	@NotBlank(message = "Confirmation Password is mandatory")
 	private String confirmationPassword;
 }
