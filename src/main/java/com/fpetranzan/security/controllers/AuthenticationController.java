1767ef8efee3facc928f93bdcc456622308068b9
fpetranzan <francesco.petranzan@gmail.com>
2024-04-17 23:42:27 +0200

    refactoring and changes for implement email verify

--- src/main/java/com/fpetranzan/security/controllers/AuthenticationController.java
+++ src/main/java/com/fpetranzan/security/controllers/AuthenticationController.java
@@ -5,12 +5,15 @@ import com.fpetranzan.security.models.auth.AuthenticationResponse;
 import com.fpetranzan.security.models.auth.RegisterRequest;
 import com.fpetranzan.security.models.auth.VerificationRequest;
 import com.fpetranzan.security.services.AuthenticationService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -22,12 +25,17 @@ public class AuthenticationController {
 	private final AuthenticationService authenticationService;
 
 	@PostMapping("/register")
-	public AuthenticationResponse register(@RequestBody @Valid RegisterRequest request) {
-		return authenticationService.register(request);
+	public void register(@RequestBody @Valid RegisterRequest request) throws MessagingException {
+		authenticationService.register(request);
+	}
+
+	@GetMapping("/activate-account")
+	public AuthenticationResponse activateAccount(@RequestParam String token) throws MessagingException {
+		return authenticationService.activateAccount(token);
 	}
 
 	@PostMapping("/authenticate")
-	public AuthenticationResponse authenticate(@RequestBody AuthenticationRequest request) {
+	public AuthenticationResponse authenticate(@RequestBody @Valid AuthenticationRequest request) {
 		return authenticationService.authenticate(request);
 	}
 
@@ -37,7 +45,7 @@ public class AuthenticationController {
 	}
 
 	@PostMapping("/verify")
-	public AuthenticationResponse verifyCode(@RequestBody VerificationRequest request) {
+	public AuthenticationResponse verifyCode(@RequestBody @Valid VerificationRequest request) {
 		return authenticationService.verifyCode(request);
 	}
 }
