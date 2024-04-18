5c224cdddb7d49b193846aa95731ed602c50bf01
fpetranzan <francesco.petranzan@gmail.com>
2024-04-18 21:57:17 +0200

    removed unused imports

--- src/main/java/com/fpetranzan/security/controllers/UserController.java
+++ src/main/java/com/fpetranzan/security/controllers/UserController.java
@@ -3,13 +3,11 @@ package com.fpetranzan.security.controllers;
 import com.fpetranzan.security.models.auth.ChangePasswordRequest;
 import com.fpetranzan.security.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.security.Principal;
 
 @RestController
