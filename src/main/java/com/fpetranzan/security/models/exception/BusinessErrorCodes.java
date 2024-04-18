553b303505e9c943d2189f19c0b0a938563745d5
fpetranzan <francesco.petranzan@gmail.com>
2024-04-18 21:48:49 +0200

    created custom error code enum

---
+++ src/main/java/com/fpetranzan/security/models/exception/BusinessErrorCodes.java
@@ -0,0 +1,33 @@
+package com.fpetranzan.security.models.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.IM_USED;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+public enum BusinessErrorCodes {
+
+	USER_EXISTS(226, IM_USED, "User already exists"),
+	PASSWORD_ERROR(400, BAD_REQUEST, "Error in the password entered"),
+	INVALID_TOKEN(400, BAD_REQUEST, "Invalid access token"),
+	BAD_CREDENTIALS(403, FORBIDDEN, "Email and / or Password is incorrect"),
+	USER_NOT_FOUND(404, NOT_FOUND, "User not found"),
+	NO_USER_FOR_TOKEN(406, NOT_ACCEPTABLE, "User not found for auth token");
+
+	@Getter
+	private final int code;
+	@Getter
+	private final String description;
+	@Getter
+	private final HttpStatus httpStatus;
+
+	BusinessErrorCodes(int code, HttpStatus status, String description) {
+		this.code = code;
+		this.description = description;
+		this.httpStatus = status;
+	}
+}
