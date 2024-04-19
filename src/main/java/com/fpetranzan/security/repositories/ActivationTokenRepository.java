ea7fddce96e0bfc41383b09ca3549cb436ba915f
fpetranzan <francesco.petranzan@gmail.com>
2024-04-19 20:46:53 +0200

    changed query for get activation token

--- src/main/java/com/fpetranzan/security/repositories/ActivationTokenRepository.java
+++ src/main/java/com/fpetranzan/security/repositories/ActivationTokenRepository.java
@@ -2,11 +2,18 @@ package com.fpetranzan.security.repositories;
 
 import com.fpetranzan.security.models.token.ActivationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ActivationTokenRepository extends JpaRepository<ActivationToken, Integer> {
 
-	Optional<ActivationToken> findByToken(String token);
+	@Query("""
+		SELECT t
+		FROM ActivationToken t
+		JOIN User u ON t.user.id = u.id
+		WHERE u.id = :userId AND t.token = :token AND t.validatedAt IS NULL
+		""")
+	Optional<ActivationToken> findByUserAndToken(Integer userId, String token);
 
 }
